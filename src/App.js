81938de15fef076af33438624cf6719614b026b2
zhangqifang123 <41544123@qq.com>
2020-04-28 15:14:31 +0800

    提交

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          go少时诵诗书所所三生三世
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
